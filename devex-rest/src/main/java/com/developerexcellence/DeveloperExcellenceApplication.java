7fc65c6fddec883952370a05661fb6bb7157f32b
David Aihe <doa9200@gmail.com>
2023-07-03 18:16:45 -0400

    main: file structure refactor

--- devex-rest/src/main/java/com/developerexcellence/DeveloperExcellenceApplication.java
+++ devex-rest/src/main/java/com/developerexcellence/DeveloperExcellenceApplication.java
@@ -1,4 +1,4 @@
-package com.devex.developerexcellence;
+package com.developerexcellence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- devex-rest/src/test/java/com/developerexcellence/DeveloperExcellenceApplicationTests.java
+++ devex-rest/src/test/java/com/developerexcellence/DeveloperExcellenceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devex.developerexcellence;
+package com.developerexcellence;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
